12c3fca6962d34d0a9fc675d2c8dffc4a8834afc
rrakman <rrakman@student_1337.ma>
2024-10-27 19:47:32 +0100

    templates exs finished

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,34 +1,84 @@
 #pragma once
+#include <iostream>
 
 template <typename T>
 class Array {
     private:
-        T* m_data;
-        unsigned int m_size;
+        T* a_data;
+        unsigned int a_size;
     public:
         Array();
         Array(unsigned int n);
-        // Array(const Array& src);
-        // const  Array & operator=(const Array &src);
+        Array(const Array& src);
+        const  Array & operator=(const Array &src);
         ~Array();
+        T& operator[](unsigned int i);
+        const T& operator[](unsigned int i) const;
+        unsigned int size() const;
 };
 
 template <typename T>
 Array<T>::Array()
 {
-    m_data = new T();
-    m_size = 0;
+    a_data = new T();
+    a_size = 0;
 }
 
 template <typename T>
-Array<T>::Array(unsigned int n)
+Array<T>::Array(unsigned int n) : a_size(n)
 {
-    m_data = new T[n]();
+    a_data = new T[a_size]();
 }
 
+template <typename T>
+Array<T>::Array(const Array &src) : a_size(src.a_size)
+{
+    a_data = new T[a_size]();
+    for (unsigned int i = 0; i < a_size; i++)
+    {
+        a_data[i] = src.a_data[i];
+    }    
+}
+
+template <typename T>
+const Array<T> & Array<T>::operator=(const Array<T> &src)
+{
+    if (this == &src)
+        return *this;
+    delete [] a_data;
+    a_size = src.a_size;
+    a_data = new T[a_size]();
+    for (unsigned int i = 0; i < a_size; i++)
+    {
+        a_data[i] = src.a_data[i];
+    }
+    return *this;    
+}
 
 template <typename T>
 Array<T>::~Array()
 {
-    delete [] m_data;
+    delete [] a_data;
 }
+
+template <typename T>
+T& Array<T>::operator[](unsigned int i)
+{
+    if (i >= a_size)
+        throw std::out_of_range("Index out of range");
+    return a_data[i];
+}
+
+template <typename T>
+const T& Array<T>::operator[](unsigned int i) const
+{
+    if (i >= a_size)
+        throw std::out_of_range("Index out of range");
+    return a_data[i];
+}
+
+template <typename T>
+unsigned int Array<T>::size() const
+{
+    return a_size;
+} 
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,6 +1,21 @@
 #include "Array.hpp"
 
 int main()
-{
-    Array<int> a(8);
+{    
+    Array<int> arr1(20);
+    for (unsigned int i = 0; i < arr1.size(); ++i)
+        arr1[i] = i + 13;
+
+    std::cout << "Array elements: ";
+    for (unsigned int i = 0; i < arr1.size(); ++i)
+        std::cout << arr1[i] << " ";
+    std::cout << std::endl;
+
+    try {
+        arr1[1] = 42;
+    } catch (const std::exception& e) {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+    }
+    
+    return 0;
 }
